ceea3942b1a6ef8b10ed3e4fc03ec7722b8d73be
xuxizhen <xuxizhen@gmail.com>
2023-03-08 09:41:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/api/generate.ts
@@ -0,0 +1,32 @@
+import type { APIRoute } from 'astro'
+import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
+import { verifySignature } from '@/utils/auth'
+// #vercel-disable-blocks
+import { fetch, ProxyAgent } from 'undici'
+// #vercel-end
+
+const apiKey = import.meta.env.OPENAI_API_KEY
+const httpsProxy = import.meta.env.HTTPS_PROXY
+const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
+
+export const post: APIRoute = async (context) => {
+  const body = await context.request.json()
+  const { sign, time, messages } = body
+  if (!messages) {
+    return new Response('No input text')
+  }
+  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '', }, sign)) {
+    return new Response('Invalid signature')
+  }
+  const initOptions = generatePayload(apiKey, messages)
+  // #vercel-disable-blocks
+  if (httpsProxy) {
+    initOptions['dispatcher'] = new ProxyAgent(httpsProxy)
+  }
+  // #vercel-end
+
+  // @ts-ignore
+  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions) as Response
+
+  return new Response(parseOpenAIStream(response))
+}
